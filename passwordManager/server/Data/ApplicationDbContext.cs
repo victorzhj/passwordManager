a4da752024b92883e709a154b3428a7059b934ff
Victor Zhou <victor.zhoujiang@tuni.fi>
2024-04-06 18:32:50 +0300

    connected server and database

---
+++ passwordManager/server/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using server.Models;
+
+namespace server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Password> Password { get; set; }
+        
+        /*
+        protected readonly IConfiguration Configuration;
+        
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = Configuration.GetConnectionString("Default");
+            Console.WriteLine($"Connection string: {connectionString}");
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+        */
+    }
+}
--- passwordManager/server/Program.cs
+++ passwordManager/server/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("Default");
+//Console.WriteLine($"Connection string: {connectionString}");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
