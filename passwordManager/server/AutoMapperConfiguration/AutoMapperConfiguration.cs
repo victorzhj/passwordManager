baa2ecdda6c973e61e87b21db8d61bb9aba62d4e
Victor Zhou <victor.zhoujiang@tuni.fi>
2024-04-08 02:10:45 +0300

    add autoMapper

---
+++ passwordManager/server/AutoMapperConfiguration/AutoMapperConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using server.Dto;
+using server.Models;
+
+namespace server.AutoMapperConfiguration
+{
+    public class AutoMapperConfiguration : Profile
+    {
+        public AutoMapperConfiguration()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+            CreateMap<UserLoginDto, User>();
+            CreateMap<User, UserLoginDto>();
+            CreateMap<UserCreationDto, User>();
+            CreateMap<User, UserCreationDto>();
+            CreateMap<Password, PasswordDto>();
+            CreateMap<PasswordDto, Password>();
+        }
+    }
+}
--- passwordManager/server/Program.cs
+++ passwordManager/server/Program.cs
@@ -1,14 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using server.Dao;
 using server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped(typeof(IGenDao<>), typeof(GenDao<>));
 var connectionString = builder.Configuration.GetConnectionString("Default");
 //Console.WriteLine($"Connection string: {connectionString}");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 
-
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
